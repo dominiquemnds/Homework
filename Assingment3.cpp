aa3ab47badbd5b0b2dd28fe7c1c42d34a33ea5bf
dominiquemnds <46972579+dominiquemnds@users.noreply.github.com>
2019-02-11 19:41:56 -0500

    Add files via upload
    I had trouble with the last hashtable in this set.

---
+++ Assingment3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <functional>
+using namespace std;
+ 
+int main() {
+int x=5;
+string mystring;
+float w= 0.5;
+bool torf= true;
+int myintarray [5];
+
+}
+std::string str = "Meet the new boss...";
+std::hash<std::string> hash_fn;
+
