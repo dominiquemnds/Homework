be1b078c85b9ec5146aca02fb51d9aeae0d1bc64
dominiquemnds <46972579+dominiquemnds@users.noreply.github.com>
2019-02-25 20:31:11 -0500

    Add files via upload
    Code for PLP4

---
+++ PLP4.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+
+int x=5;
+int y=10;
+
+if (x<y)
+	cout<<"The if statement works"<<endl;
+
+
+if(x<y and x>0)
+	cout<<"The multi-conditional if statement works"<<endl;
+
+while(x<y){
+	x++;
+	cout<<x<<endl;
+	}
+cout<<"The while statement works"<<endl;
+	
+do{
+	y++;
+	cout<<y<<endl;
+	
+}
+while(y<20);
+
+cout<<"The do while works"<<endl;
+	
+for(int i=0;i<10;i++){
+	if(i==9){
+		cout<<"This for loop works"<<endl;
+	}
+}
+
+/*int arr[] = { 10, 20, 30, 40 }; 
+
+    for (int x : arr) 
+        cout << x << endl;
+        
+cout<<"The for-each loop works"<<endl;
+
+*/
+switch(1){
+	case 1: cout<<"This switch statement works!";
+	continue;
+	case 2: cout<<"This code should execute";
+	break;
+}
+}
+
+
